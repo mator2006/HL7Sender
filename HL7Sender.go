4f3fcf23609085d545bdc4592997f28ed2a4375c
mator2006 <mator2006@gmail.com>
2018-12-13 15:30:07 +0800

    1213

--- HL7Sender.go
+++ HL7Sender.go
@@ -29,22 +29,13 @@ func main() {
 		SPSDESC,
 		StationNAME,
 		StationAET,
-		configurefile string
+		configurefile,
+		StudyInstanceUID	string
 
-	iniexist, err := os.Stat("config.ini")
-	//jsonexist,err := os.Stat ("config.json")
-	//yamlexist,err := os.Stat ("config.yaml")
+	configurefile = "config.ini"
+	iniexist, err := os.Stat(configurefile)
 
-	if iniexist != nil /* || jsonexist != nil|| yamlexist != nil*/ {
-		if iniexist != nil {
-			configurefile = "config.ini"
-		}
-		/*if jsonexist != nil {
-			configurefile = "config.json"
-		}
-		if yamlexist != nil {
-			configurefile = "config.yaml"
-		}*/
+	if iniexist != nil {
 		fmt.Println("Found Configure file.\nConfigure file is [" + configurefile + "].")
 		config := struct {
 			Netconn struct {
@@ -52,18 +43,19 @@ func main() {
 				Port string
 			}
 			HL7order struct {
-				PatientID    string
-				PatientNAME  string
-				PatientBD    string
-				PatientSEX   string
-				OrderControl string
-				AccessionNO  string
-				RPNO         string
-				SPNO         string
-				MODALITY     string
-				SPSDESC      string
-				StationNAME  string
-				StationAET   string
+				PatientID    		string
+				PatientNAME  		string
+				PatientBD    		string
+				PatientSEX   		string
+				OrderControl 		string
+				AccessionNO  		string
+				RPNO         		string
+				SPNO         		string
+				MODALITY     		string
+				SPSDESC      		string
+				StationNAME  		string
+				StationAET   		string
+				StudyInstanceUID	string
 			}
 		}{}
 
@@ -78,8 +70,7 @@ func main() {
 			Port = strings.TrimSpace(config.Netconn.Port)
 
 			PatientID = strings.TrimSpace(config.HL7order.PatientID)
-			PatientNAME = strings.TrimSpace(config.HL7order.PatientNAME)
-			PatientNAME = strings.Replace(PatientNAME, " ", "^", -1)
+			PatientNAME = strings.Replace(strings.TrimSpace(config.HL7order.PatientNAME), " ", "^", -1)
 			PatientBD = strings.TrimSpace(config.HL7order.PatientBD)
 			PatientSEX = strings.TrimSpace(config.HL7order.PatientSEX)
 			OrderControl = strings.TrimSpace(config.HL7order.OrderControl)
@@ -90,9 +81,11 @@ func main() {
 			SPSDESC = strings.TrimSpace(config.HL7order.SPSDESC)
 			StationNAME = strings.TrimSpace(config.HL7order.StationNAME)
 			StationAET = strings.TrimSpace(config.HL7order.StationAET)
+			StudyInstanceUID = strings.TrimSpace(config.HL7order.StudyInstanceUID)
+			
 		}
 	} else {
-		fmt.Println("Not found configure file !\nUse DEFAULT config!!!")
+		fmt.Println("Not found configure file !\nUse DEFAULT config!")
 
 		fmt.Println("Please enter HL7 server IP address:")
 		input := bufio.NewScanner(os.Stdin)
@@ -108,10 +101,22 @@ func main() {
 		input = bufio.NewScanner(os.Stdin)
 		input.Scan()
 		Port = input.Text()
-		fmt.Println(Host, Port)
-		if Port == "" {
+
+		Portd := false
+		if Port != "" {
+			Porttoi, err := strconv.Atoi(Port)
+			if err == nil {
+				if Porttoi > 0 && Porttoi < 65536 {
+					Port = strconv.Itoa(Porttoi)
+					Portd = true
+				}
+			}
+		}
+		if !Portd {
+			fmt.Println("Invalid Port number\nUse Default Port [2575]")
 			Port = "2575"
 		}
+		_ = Portd
 
 		rand.Seed(time.Now().Unix())
 		DATESN := time.Now().Format("20060102")
@@ -129,6 +134,7 @@ func main() {
 		SPSDESC = "Tou Lu"
 		StationNAME = "NO3RF"
 		StationAET = "AW44"
+		StudyInstanceUID = "2.16.840.1.113929.1.2.6493."+DATESN+"."+QUEENSN+".762142" //https://blog.csdn.net/dragonlet/article/details/37052997
 	}
 
 	_ = iniexist
@@ -136,11 +142,11 @@ func main() {
 	//_=yamlexist
 	hl7mesgsub = //此处修改需谨慎
 		`MSH|^~\&|||||||ORM^O01||P|2.3|||||CN` + "\r" +
-			`PID|||` + PatientID + `^^^HL7||` + PatientNAME + `^3||` + PatientBD + `|` + PatientSEX + `|||||||||||||||||||||||||||||||` + "\r" +
+			`PID|||` + PatientID + `^^^HIS||` + PatientNAME + `||` + PatientBD + `|` + PatientSEX + `|||||||||||||||||||||||||||||||` + "\r" +
 			`PV1||E|||||||||||||||||V103-1^^^ADT1||||||||||||||||||||||||||||||||V|` + "\r" +
 			`ORC|` + OrderControl + `||||SC||1^once^^^^S||||||||||||` + "\r" +
 			`OBR|1|||^^^^` + SPSDESC + `||||||||||||||` + AccessionNO + `|` + RPNO + `|` + SPNO + `||||` + MODALITY + `|||1^once^^^^S|||WALK||||||||||||||P1^` + SPSDESC + `^ERL_MESA, ZDS|1.113654.3.13.1025^100^Application^DICOM` + "\r" +
-			`ZDS|2.16.840.1.113929.1.2.6493.20070508.21948.762142^DCM4CHEE^Application^DICOM|` + StationNAME + `|` + StationAET + "\r"
+			`ZDS|`+StudyInstanceUID+`^MEPACS^Application^DICOM|` + StationNAME + `|` + StationAET + "\r"
 	hl7mesg = "\v" + hl7mesgsub + "\x1C" + "\r" //谨慎同上
 
 	//net.dial 拨号 获取tcp连接
@@ -150,24 +156,28 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("获取[" + constr + "]的tcp连接成功...")
+	fmt.Println("获取[" + constr + "]的tcp连接成功.")
 
 	//发送HL7数据
-	fmt.Fprintf(conn, hl7mesg)
-	fmt.Println("发送hl7数据完成...")
+	sendbytecount, err := fmt.Fprintf(conn, hl7mesg)
+	if err != nil {
+		fmt.Println("发送错误！", err)
+		return
+	}
+	fmt.Println("共发送", sendbytecount, "字节.\n发送hl7数据完成.")
 
 	//接收返回数据
 	Receivemesg, _ := bufio.NewReader(conn).ReadString('\x1C') //读取返回数据，遇到指定字符结束
 
 	if !strings.Contains(string(Receivemesg), `AR`) { //不知道为什么，直接写包含AA就是不行
-		fmt.Println("传输成功")
+		fmt.Println("传输成功.")
 	} else {
-		fmt.Println("失败，未知错误")
+		fmt.Println("失败，未知错误!")
 		fmt.Println(string(Receivemesg))
 	}
 	//关闭连接
 	defer conn.Close()
-	fmt.Println("连接已关闭")
+	fmt.Println("连接已关闭.")
 }
 
 //OBR|1|||| to OBR|1|||^^^^`+SPSDESC+`| mator 2018.12.13

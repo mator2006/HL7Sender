324a100af8d7f9179c667eaf340a3c3943c99b70
mator2006 <mator2006@gmail.com>
2018-12-15 14:59:48 +0800

    da gai fmt

--- HL7Sender.go
+++ HL7Sender.go
@@ -12,47 +12,47 @@ import (
 	"time"
 )
 
-type hl7 struct{
-	Host				string
-	Port				string
-	hl7mesg				string
-	hl7mesgsub			string
-	PatientNAME			string
-	PatientBD			string
-	PatientSEX			string
-	PatientID			string
-	OrderControl		string
-	AccessionNO			string
-	RPNO, SPNO			string
-	MODALITY			string
-	SPSDESC				string
-	StationNAME			string
-	StationAET			string
-	configurefile		string
-	StudyInstanceUID	string
+type hl7 struct {
+	Host             string
+	Port             string
+	hl7mesg          string
+	hl7mesgsub       string
+	PatientNAME      string
+	PatientBD        string
+	PatientSEX       string
+	PatientID        string
+	OrderControl     string
+	AccessionNO      string
+	RPNO, SPNO       string
+	MODALITY         string
+	SPSDESC          string
+	StationNAME      string
+	StationAET       string
+	configurefile    string
+	StudyInstanceUID string
 }
 
 func main() {
 
-	var hl7data	hl7
+	var hl7data hl7
 
 	configurefile := "config.ini"
 	iniexist, err := os.Stat(configurefile)
 
 	if iniexist != nil {
-		useconfigfile(hl7data,configurefile)
+		useconfigfile(hl7data, configurefile)
 	} else {
 		usedefconfig(hl7data)
 	}
-	
-	_=err
+
+	_ = err
 	_ = iniexist
 
 	addhl7data(hl7data)
 	senddata(hl7data)
 }
 
-func useconfigfile(hl7data hl7,configurefile string){
+func useconfigfile(hl7data hl7, configurefile string) {
 	fmt.Println("Found Configure file.\nConfigure file is [" + configurefile + "].")
 	config := struct {
 		Netconn struct {
@@ -60,19 +60,19 @@ func useconfigfile(hl7data hl7,configurefile string){
 			Port string
 		}
 		HL7order struct {
-			PatientID    		string
-			PatientNAME  		string
-			PatientBD    		string
-			PatientSEX   		string
-			OrderControl 		string
-			AccessionNO  		string
-			RPNO         		string
-			SPNO         		string
-			MODALITY     		string
-			SPSDESC      		string
-			StationNAME  		string
-			StationAET   		string
-			StudyInstanceUID	string
+			PatientID        string
+			PatientNAME      string
+			PatientBD        string
+			PatientSEX       string
+			OrderControl     string
+			AccessionNO      string
+			RPNO             string
+			SPNO             string
+			MODALITY         string
+			SPSDESC          string
+			StationNAME      string
+			StationAET       string
+			StudyInstanceUID string
 		}
 	}{}
 
@@ -82,27 +82,27 @@ func useconfigfile(hl7data hl7,configurefile string){
 		fmt.Println(err)
 		fmt.Println("You can DO THIS:\n1.Tyr modify the configure file:[" + configurefile + "]\n2.Or remove this configure files,Will use DEFAULT configure")
 		return
-		} else {
-			hl7data.Host = strings.TrimSpace(config.Netconn.Host)
-			hl7data.Port = strings.TrimSpace(config.Netconn.Port)
+	} else {
+		hl7data.Host = strings.TrimSpace(config.Netconn.Host)
+		hl7data.Port = strings.TrimSpace(config.Netconn.Port)
 
-			hl7data.PatientID = strings.TrimSpace(config.HL7order.PatientID)
-			hl7data.PatientNAME = strings.Replace(strings.TrimSpace(config.HL7order.PatientNAME), " ", "^", -1)
-			hl7data.PatientBD = strings.TrimSpace(config.HL7order.PatientBD)
-			hl7data.PatientSEX = strings.TrimSpace(config.HL7order.PatientSEX)
-			hl7data.OrderControl = strings.TrimSpace(config.HL7order.OrderControl)
-			hl7data.AccessionNO = strings.TrimSpace(config.HL7order.AccessionNO)
-			hl7data.RPNO = strings.TrimSpace(config.HL7order.RPNO)
-			hl7data.SPNO = strings.TrimSpace(config.HL7order.SPNO)
-			hl7data.MODALITY = strings.TrimSpace(config.HL7order.MODALITY)
-			hl7data.SPSDESC = strings.TrimSpace(config.HL7order.SPSDESC)
-			hl7data.StationNAME = strings.TrimSpace(config.HL7order.StationNAME)
-			hl7data.StationAET = strings.TrimSpace(config.HL7order.StationAET)
-			hl7data.StudyInstanceUID = strings.TrimSpace(config.HL7order.StudyInstanceUID)
-			}
+		hl7data.PatientID = strings.TrimSpace(config.HL7order.PatientID)
+		hl7data.PatientNAME = strings.Replace(strings.TrimSpace(config.HL7order.PatientNAME), " ", "^", -1)
+		hl7data.PatientBD = strings.TrimSpace(config.HL7order.PatientBD)
+		hl7data.PatientSEX = strings.TrimSpace(config.HL7order.PatientSEX)
+		hl7data.OrderControl = strings.TrimSpace(config.HL7order.OrderControl)
+		hl7data.AccessionNO = strings.TrimSpace(config.HL7order.AccessionNO)
+		hl7data.RPNO = strings.TrimSpace(config.HL7order.RPNO)
+		hl7data.SPNO = strings.TrimSpace(config.HL7order.SPNO)
+		hl7data.MODALITY = strings.TrimSpace(config.HL7order.MODALITY)
+		hl7data.SPSDESC = strings.TrimSpace(config.HL7order.SPSDESC)
+		hl7data.StationNAME = strings.TrimSpace(config.HL7order.StationNAME)
+		hl7data.StationAET = strings.TrimSpace(config.HL7order.StationAET)
+		hl7data.StudyInstanceUID = strings.TrimSpace(config.HL7order.StudyInstanceUID)
 	}
+}
 
-func usedefconfig(hl7data hl7){
+func usedefconfig(hl7data hl7) {
 	fmt.Println("Not found configure file !\nUse DEFAULT config!")
 
 	fmt.Println("Please enter HL7 server IP address:")
@@ -113,7 +113,7 @@ func usedefconfig(hl7data hl7){
 	if vHost == "" || net.ParseIP(vHost) == nil {
 		fmt.Println("Invalid IP address.\nEnd.")
 		return
-	}else{
+	} else {
 		hl7data.Host = vHost
 	}
 
@@ -148,27 +148,27 @@ func usedefconfig(hl7data hl7){
 	hl7data.PatientSEX = "M"
 	hl7data.OrderControl = "NW"
 	hl7data.AccessionNO = DATESN + QUEENSN
-	hl7data.RPNO = "RP" + hl7data.AccessionNO[2:len(DATESN + QUEENSN)]
-	hl7data.SPNO = "SP" + hl7data.AccessionNO[2:len(DATESN + QUEENSN)]
+	hl7data.RPNO = "RP" + hl7data.AccessionNO[2:len(DATESN+QUEENSN)]
+	hl7data.SPNO = "SP" + hl7data.AccessionNO[2:len(DATESN+QUEENSN)]
 	hl7data.MODALITY = "MR"
 	hl7data.SPSDESC = "Tou Lu"
 	hl7data.StationNAME = "NO3RF"
 	hl7data.StationAET = "AW44"
-	hl7data.StudyInstanceUID = "2.16.840.1.113929.1.2.6493."+DATESN+"."+QUEENSN+".762142" //https://blog.csdn.net/dragonlet/article/details/37052997
+	hl7data.StudyInstanceUID = "2.16.840.1.113929.1.2.6493." + DATESN + "." + QUEENSN + ".762142" //https://blog.csdn.net/dragonlet/article/details/37052997
 }
 
-func addhl7data(hl7data hl7){
-	hl7data.hl7mesgsub = `MSH|^~\&|||||||ORM^O01||P|2.3|||||CN`+"\r"+`PID|||` + hl7data.PatientID + `^^^HIS||`+hl7data.PatientNAME + `||` +hl7data.PatientBD + `|` + hl7data.PatientSEX + `|||||||||||||||||||||||||||||||` + "\r" +
+func addhl7data(hl7data hl7) {
+	hl7data.hl7mesgsub = `MSH|^~\&|||||||ORM^O01||P|2.3|||||CN` + "\r" + `PID|||` + hl7data.PatientID + `^^^HIS||` + hl7data.PatientNAME + `||` + hl7data.PatientBD + `|` + hl7data.PatientSEX + `|||||||||||||||||||||||||||||||` + "\r" +
 		`PV1||E|||||||||||||||||V103-1^^^ADT1||||||||||||||||||||||||||||||||V|` + "\r" +
 		`ORC|` + hl7data.OrderControl + `||||SC||1^once^^^^S||||||||||||` + "\r" +
 		`OBR|1|||^^^^` + hl7data.SPSDESC + `||||||||||||||` + hl7data.AccessionNO + `|` + hl7data.RPNO + `|` + hl7data.SPNO + `||||` + hl7data.MODALITY +
-		 `|||1^once^^^^S|||WALK||||||||||||||P1^` + hl7data.SPSDESC + `^ERL_MESA, ZDS|1.113654.3.13.1025^100^Application^DICOM` + "\r" +
-		`ZDS|`+hl7data.StudyInstanceUID+`^MEPACS^Application^DICOM|` + hl7data.StationNAME + `|` + hl7data.StationAET + "\r"
-		hl7data.hl7mesg = "\v" + hl7data.hl7mesgsub + "\x1C" + "\r" //谨慎同上
+		`|||1^once^^^^S|||WALK||||||||||||||P1^` + hl7data.SPSDESC + `^ERL_MESA, ZDS|1.113654.3.13.1025^100^Application^DICOM` + "\r" +
+		`ZDS|` + hl7data.StudyInstanceUID + `^MEPACS^Application^DICOM|` + hl7data.StationNAME + `|` + hl7data.StationAET + "\r"
+	hl7data.hl7mesg = "\v" + hl7data.hl7mesgsub + "\x1C" + "\r" //谨慎同上
 	fmt.Println(hl7data.hl7mesgsub)
 }
 
-func senddata (hl7data hl7){
+func senddata(hl7data hl7) {
 	//net.dial 拨号 获取tcp连接
 	constr := hl7data.Host + ":" + hl7data.Port
 	conn, err := net.Dial("tcp", constr)
@@ -199,7 +199,6 @@ func senddata (hl7data hl7){
 	defer conn.Close()
 	fmt.Println("连接已关闭.")
 }
-
 
 //OBR|1|||| to OBR|1|||^^^^`+SPSDESC+`| mator 2018.12.13
 // test git commit 5
